0e23ab846c0746c71e98f9c55969d7ac9283204f
=Marcos Acosta Torrissi <marcos.acosta.em@gmail.com>
2020-12-02 23:02:33 -0300

    BBDD actualizada

--- db_productos.sql
+++ db_productos.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-12-2020 a las 14:48:19
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.6
+-- Tiempo de generación: 03-12-2020 a las 03:01:34
+-- Versión del servidor: 10.4.13-MariaDB
+-- Versión de PHP: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,17 +29,16 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categoria` (
   `id_categoria` int(11) NOT NULL,
-  `nombre` varchar(150) NOT NULL,
-  `url_imagen` text NOT NULL
+  `nombre` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `categoria`
 --
 
-INSERT INTO `categoria` (`id_categoria`, `nombre`, `url_imagen`) VALUES
-(1, 'BEBIDAS', 'https://i.ibb.co/BPTw7GB/beer.jpg'),
-(2, 'COMIDAS', 'https://i.ibb.co/rp1DKz6/bbq.jpg');
+INSERT INTO `categoria` (`id_categoria`, `nombre`) VALUES
+(1, 'BEBIDAS'),
+(2, 'COMIDAS');
 
 -- --------------------------------------------------------
 
@@ -61,8 +60,8 @@ CREATE TABLE `comentario` (
 
 INSERT INTO `comentario` (`id`, `texto`, `ranking`, `id_usser`, `id_producto`) VALUES
 (5, 'Para chuparse los dedos', 4, 2, 7),
-(10, 'Malardo', 2, 2, 1),
-(12, 'very rico', 5, 2, 7);
+(12, 'very rico', 5, 2, 7),
+(19, 'Les recomiendo esto!', 4, 3, 11);
 
 -- --------------------------------------------------------
 
@@ -75,27 +74,27 @@ CREATE TABLE `producto` (
   `nombre` varchar(150) NOT NULL,
   `descripcion` text NOT NULL,
   `precio` double NOT NULL,
-  `id_categoria` int(11) NOT NULL
+  `id_categoria` int(11) NOT NULL,
+  `imagen` varchar(50) CHARACTER SET latin1 NOT NULL DEFAULT 'images/sin-imagen.jpg'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
-INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `precio`, `id_categoria`) VALUES
-(1, 'puro humo', 'Picada de salames, quesos y fiambres de la región. Todo ahumado.', 250, 2),
-(2, 'sandwich tandilense ', 'Un clásico. Doble porción de bondiola de cerdo acompañada con rúcula, tomates asados y cebolla caramelizada. Todo bañado en salsa de mostaza a la miel.', 200, 2),
-(3, 'provoleta alioli', 'Clásica provoleta a la parrilla con un toque de ajo y oliva.', 180, 2),
-(4, 'nachos con queso', 'Nachos caseros con dip de Cheddar.', 250, 2),
-(5, 'veggie style', 'Milanesa de berenjena con mix de vegetales asados en pan casero con semillas.', 220, 2),
-(6, 'cata de morcipan', 'Tres tipos de morcilla en tres tipos de panes diferentes. Nada más que agregar.', 300, 2),
-(7, 'papas refugio', 'Papas fritas en grasa con crema de panceta, verdeo y queso.', 260, 2),
-(8, 'Ensalada rustica ', 'Lechuga, tomate, zanahoria, cebolla y lo que tengamos en la huerta.', 300, 2),
-(9, 'indian pale ale', 'Cerveza rubia, moderado alcohol y amargor. Es la versión artesanal de las cervezas rubias masivas industriales, más sabrosa e intensa. IBU 18 ALC% 4.5', 195, 1),
-(11, 'cream stout', 'De un profundo color oscuro con aromas tostado y café. Presenta un sabor intenso y un cuerpo y espuma cremosa. IBU 18 ALC% 4.5', 180, 1),
-(12, 'amber lager', 'Sus maltas le otorgan un color ámbar rojizo y un sabor balanceado entre el dulzor y el amargor. IBU 18 ALC% 4.5', 180, 1),
-(14, 'pink apa', 'De bajo contenido alcohólico, fuerte presencia en aroma y gusto de lúpulo americano. Destacan notas cítricas y de frutas tropicales. IBU 35 ALC% 3.5', 190, 1),
-(16, 'lager', 'Con un aroma frutado, un atractivo color dorado y un inconfundible sabor fresco e intenso. IBU 18 ALC% 4.8', 175, 1);
+INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `precio`, `id_categoria`, `imagen`) VALUES
+(2, 'sandwich tandilense ', 'Un clásico. Doble porción de bondiola de cerdo acompañada con rúcula, tomates asados y cebolla caramelizada. Todo bañado en salsa de mostaza a la miel.', 200, 2, 'images/5fc81a0d0963b.jpg'),
+(3, 'provoleta alioli', 'Clásica provoleta a la parrilla con un toque de ajo y oliva.', 180, 2, 'images/sin-imagen.jpg'),
+(5, 'veggie style', 'Milanesa de berenjena con mix de vegetales asados en pan casero con semillas.', 220, 2, 'images/sin-imagen.jpg'),
+(6, 'cata de morcipan', 'Tres tipos de morcilla en tres tipos de panes diferentes. Nada más que agregar.', 300, 2, 'images/sin-imagen.jpg'),
+(7, 'papas refugio', 'Papas fritas en grasa con crema de panceta, verdeo y queso.', 260, 2, 'images/sin-imagen.jpg'),
+(8, 'Ensalada rustica ', 'Lechuga, tomate, zanahoria, cebolla y lo que tengamos en la huerta.', 300, 2, 'images/sin-imagen.jpg'),
+(9, 'indian pale ale', 'Cerveza rubia, moderado alcohol y amargor. Es la versión artesanal de las cervezas rubias masivas industriales, más sabrosa e intensa. IBU 18 ALC% 4.5', 195, 1, 'images/sin-imagen.jpg'),
+(11, 'cream stout', 'De un profundo color oscuro con aromas tostado y café. Presenta un sabor intenso y un cuerpo y espuma cremosa. IBU 18 ALC% 4.5', 180, 1, 'images/sin-imagen.jpg'),
+(12, 'amber lager', 'Sus maltas le otorgan un color ámbar rojizo y un sabor balanceado entre el dulzor y el amargor. IBU 18 ALC% 4.5', 180, 1, 'images/sin-imagen.jpg'),
+(14, 'pink apa', 'De bajo contenido alcohólico, fuerte presencia en aroma y gusto de lúpulo americano. Destacan notas cítricas y de frutas tropicales. IBU 35 ALC% 3.5', 190, 1, 'images/sin-imagen.jpg'),
+(16, 'lager', 'Con un aroma frutado, un atractivo color dorado y un inconfundible sabor fresco e intenso. IBU 18 ALC% 4.8', 175, 1, 'images/sin-imagen.jpg'),
+(19, 'Papas', 'Papas más ricas que las anteriores', 250, 2, 'images/5fc84478c7cc8.jpg');
 
 -- --------------------------------------------------------
 
@@ -160,25 +159,25 @@ ALTER TABLE `usser`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `comentario`
 --
 ALTER TABLE `comentario`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `usser`
 --
 ALTER TABLE `usser`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
